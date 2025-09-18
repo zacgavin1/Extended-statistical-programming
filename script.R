fdbdc2603a202ae3f28bc37a8f8a012dfad6df9d
Zachary Gavin <zacharygavin@Zacharys-MacBook-Air-2.local>
2025-09-18 17:15:30 +0100

    its making me do this idk why

--- script.R
+++ script.R
@@ -1,1 +1,7 @@
-# My first R script
+x <- c("10","2","7","89","43","1") ## example vector
+ii <- which(nchar(x)>1) # where the double digits are
+xs <- rep("", length(ii)+length(x))
+iis <- ii+ 1:length(ii) # each element gets shifted one more over than the previous one
+xs[iis]<- substr(x[ii],2,2)
+xs[-iis]<- substr(x,1,1)
+xs
