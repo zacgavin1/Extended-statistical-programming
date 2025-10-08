93c59d41bc66b55987e59f42d63b5cbb0687c582
Zachary Gavin <zacharygavin@Zacharys-MacBook-Air-2.local>
2025-10-08 09:53:27 +0100

    h added to the get.net function

--- proj2.r
+++ proj2.r
@@ -1,21 +1,30 @@
-n <- 100
+n <- 10000
 people <- 1:n
 h_max <- 5
 
 set.seed(13)
 
 #### Putting people in households
-sizes = sample(1:h_max, 1000, replace=TRUE) # generates 1000 household sizes uniform from{1,2,3,4,5}
+# this does use more computation than necessary, but its such a tiny prop. of 
+# the total computation here it doesn't really make any difference overall.
+sizes = sample(1:h_max, 1000, replace=TRUE) # generates 1000 household sizes uniform from {1,2,3,4,5}
 h <- rep(1:length(sizes), sizes) # repeats i sizes[i] times. 
 h <- h[1:n] # takes only the first n of these
 
 links <- cbind(person=people, household=h)
 
+# system.time seems to suggest this is roughly O(n^2). About 8s for n=10000
 ### get.net function
 beta <- runif(n,0,1)
-nc=15 # get rid of his
-get.net <- function(beta, nc=15){
+nc=15 # get rid of this
+get.net <- function(beta, h, nc=15){
   n <- length(beta)
+  
+  # this is a bit of a hack to make a "household matrix" from h
+  # the H[i,j] is perfect sqaure only if h[i]==h[j], ie if i,j from same hshld
+  H <- matrix(rep(0,n^2),n,n)
+  H[sqrt(outer(h,h))%%1==0]<-1
+  diag(H) <- 0
   
   b_bar <- sum(beta)/n
   M_prob <-  nc*outer(beta, beta)/(b_bar^2 *(n-1))
@@ -26,12 +35,27 @@ get.net <- function(beta, nc=15){
   M_cons[upper.tri(M_cons)] <- v_cons
   
   M_cons <- M_cons + t(M_cons)
+  M <- (!H)*M_cons  # removing family connections by multiplying by negated household matrix
   conns_list <- as.list(as.data.frame(M_cons))
+  
 }
 
 
+nseir <- function(beta, h, alink, alpha=c(.1,.01,.01), delta=.2, gamma=.4, nc=15, nt=100, pinf=.005){
+  n <- length(beta)
+  x <- rep(0,n)  # everyone starts out in susceptible state
+  x[sample(1:n, pinf*n)] <- 2 # random subset of specified size is infected
+  
+  for(i in 1:nt){
+    u <- runif(n) # random variables to compare against
+    x[x==2&u<delta] <- 3
+    x[x==1&u<gamma] <- 2
+    
+    
+  }
+}
 
 
-connections <- get.net(beta)
+alink <- get.net(beta, h) # alink is the variable we need to put into nseir
 
 
