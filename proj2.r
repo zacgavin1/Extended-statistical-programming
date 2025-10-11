1def96b83fa85e36a7b2f0423db5c2b4464db94c
bcaus <brandoncausing@gmail.com>
2025-10-11 01:13:14 +0100

    tried to optimize nseir, please test on n = 10000

--- proj2.r
+++ proj2.r
@@ -57,18 +57,19 @@ get.net <- function(beta, h, nc=15){
   
   M_cons <- M_cons + t(M_cons)
   M <- (!H)*M_cons  #moving family connections by multiplying by negated household matrix
-  conns_list <- as.list(as.data.frame(matrix(M_cons==1, n,n)))
-  i_list <- lapply(conns_list, which )
+  conns_list <- as.list(as.data.frame(matrix(M_cons==1, n, n)))
+  i_list <- lapply(conns_list, which)
   
 }
 
-get.net2 <- function(beta, h, nc=15){
+get.net2 <- function(beta, h, nc=15) {
+  
   n <- length(h)
   b_bar <- sum(beta)/length(beta) 
   conns_init <- list()
   conns <- vector(mode="list", length=n)
-  for (i in 1:n){
-    b <- rbinom(n-i, 1, nc*beta[i]*beta[i+1:n]/(b_bar^2 *(n-1)) )
+  for (i in 1:n) {
+    b <- rbinom(n-i, 1, nc*beta[i]*beta[i+1:n]/(b_bar^2*(n-1)) )
     conns_init[[i]] <- which(b==1) + i 
   }
   
@@ -90,7 +91,12 @@ alink <- get.net(beta, h) # alink is the variable we need to put into nseir
 alink2 <- get.net2(beta,h)
 
 
-nseir <- function(beta, h, alink, alpha=c(.1,.01,.01), delta=.2, gamma=.4, nc=15, nt=100, pinf=.005){
+nseir <- function(beta, h, alink, 
+                  alpha=c(0.1, 0.01, 0.01), 
+                  delta=.2, 
+                  gamma=.4, 
+                  nc=15, nt=100, pinf=.005) {
+  
   n <- length(beta)
   x <- rep(0,n)  # everyone starts out in susceptible state
   num_to_infect <- max(1, round(pinf * n))
@@ -164,6 +170,109 @@ nseir <- function(beta, h, alink, alpha=c(.1,.01,.01), delta=.2, gamma=.4, nc=15
   return(list(t = t, S = S, E = E, I = I, R = R))
 }
 
+
+
+###### optimized version? ########
+nseir <- function(beta, h, alink, 
+                  alpha=c(0.1, 0.01, 0.01), 
+                  delta=.2, 
+                  gamma=.4, 
+                  nc=15, nt=100, pinf=.005) {
+  
+  n <- length(beta)
+  x <- rep(0,n)  # everyone starts out in susceptible state
+  num_to_infect <- max(1, round(pinf*n))
+  x[sample(1:n, num_to_infect)] <- 2 # random subset of specified size is infected
+  
+  # list of who shares a household with each individual
+  hh_index_list <- split(1:n, h) 
+  # remove self from each item in list
+  hh_infect <- vector(mode = "list", length = n)
+  for (i in 1:n) {
+    hh_infect[[i]] <- setdiff(hh_index_list[[h[i]]], i)
+  }
+  
+  b_bar <- sum(beta)/n
+  # general mixing daily prob of infection
+  mix_prob <-  nc*(beta %o% beta)/(b_bar^2 *(n-1)) # %o% upper triangular mult
+  
+  # vector of upper triangular els of mix_prob
+  v_prob <- mix_prob[upper.tri(mix_prob)] 
+  #rm(mix_prob) # save memory
+  
+  # initialize i,j pairs of upper triangular matrix
+  ij <- which(upper.tri(matrix(1, n, n)), arr.ind = TRUE)
+  
+  t <- S <- E <- I <- R <- rep(0, nt)
+  t[1] <- 1
+  S[1] <- n - num_to_infect
+  I[1] <- num_to_infect
+  
+  for(i in 2:nt) {
+    
+    u <- runif(n) # random variables to compare against
+    
+    ## random mixing set up
+    v_mix <- rbinom(length(v_prob), 1, v_prob) # generate bernoullis 
+    
+    infected_indices <- which(x == 2)
+    
+    # who interacted with someone infected through random mixing
+    if (length(infected_indices) > 0) {
+      mix_pairs <- ij[v_mix == 1, , drop = FALSE]
+      mix_infected <- unique(c(
+        mix_pairs[mix_pairs[ , 1] %in% infected_indices, 2],
+        mix_pairs[mix_pairs[ , 2] %in% infected_indices, 1]
+      ))
+    } else {
+      mix_infected <- integer(0)
+    }
+    
+    # if in state I (2), prob = delta -> R
+    # if in state E (1), prob = gamma -> I
+    x[x == 2 & u < delta] <- 3
+    x[x == 1 & u < gamma] <- 2
+    
+    # if in state S (0),
+    # -- prob = a_h -> I (2), if in household
+    # -- prob = a_c -> I (2), if in network
+    # -- prob = a_r*P(mixing) -> I (2), regardless
+    newly_exposed <- integer(0)
+    
+    if (length(infected_indices) > 0) {
+      # indices of those in household of someone infected
+      hh_exposed <- unique(unlist(hh_infect[infected_indices]))
+      hh_exposed <- hh_exposed[x[hh_exposed] == 0 & u[hh_exposed] < alpha[1]]
+      
+      # indices of those in regular network of someone infected
+      net_exposed <- unique(unlist(alink[infected_indices]))
+      net_exposed <- net_exposed[x[net_exposed] == 0 & u[net_exposed] < alpha[2]]
+      
+      # indices of those in contact with someone infected via random mixing
+      mix_exposed <- mix_infected[x[mix_infected] == 0 & u[mix_infected] < alpha[3]]
+      
+      # all (unique) indices of those that went from S to E
+      newly_exposed <- unique(c(hh_exposed, net_exposed, mix_exposed))  
+    }
+    
+    x[newly_exposed] <- 1
+    
+    
+    S[i] <- sum(x == 0)
+    E[i] <- sum(x == 1)
+    I[i] <- sum(x == 2)
+    R[i] <- sum(x == 3)
+    t[i] <- i
+    
+  }
+  return(list(t = t, S = S, E = E, I = I, R = R))
+}
+
+
+
+
+
+
 # default parameters
 epi <- nseir(beta, h, alink)
 
@@ -200,12 +309,3 @@ epi_plot(beta, h, alink, title = "Epidemic with Default Parameters")
 epi_plot(beta, h, alink,
          alpha = c(0.5, 0.3, 0.1), gamma = 0.5, delta = 0.1,
          title = "Epidemic with Adjusted Parameters")
-
-
-
-berngen<- function(h, n, beta){
-b_bar <- sum(beta)/n
-M_prob <-  nc*outer(beta, beta)/(b_bar^2 *(n-1))
-v_prob <- M_prob[upper.tri(M_prob)] # getting vector of upper triangular els of M_prob
-v_cons <- rbinom(length(v_prob), 1, v_prob) # generate bernoullis 
-}
