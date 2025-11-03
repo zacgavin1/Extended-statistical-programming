a55bccd09c2425f7287716c8bd1029fc4c9aafd4
shaehroz <64203537+shaehroz@users.noreply.github.com>
2025-11-03 20:27:41 +0000

    Changed q1 to allow overspilling

--- proj3.R
+++ proj3.R
@@ -1,31 +1,35 @@
 library(splines)
 
-### Question1 
-
 modelling <- function(t, K) {
-
-  knots <- seq(min(t), max(t), length.out = K - 2)
-  all_knots <- seq(min(t), max(t), length.out = K + 4)
-  X_tilde <- splineDesign(all_knots, t, outer.ok=TRUE) ##need outer.ok = TRUE to allow x to be outside the inner knots
+  range_t <- range(t)
+  internal_knots <- seq(range_t[1], range_t[2], length.out = K - 2)
+  all_knots <- c(seq(range_t[1] - 3, range_t[1], length.out = 2),internal_knots, seq(range_t[2], range_t[2] + 3, length.out = 2))
   
+  X_tilde <- splineDesign(all_knots, t, ord = 4, outer.ok = TRUE)
   
   d <- 1:80; edur <- 3.151; sdur <- .469
-  pd <- dlnorm(d, edur, sdur);
-  pd <- pd/sum(pd)
+  pd <- dlnorm(d, edur, sdur); pd <- pd / sum(pd)
   
   n <- length(t)
   X <- matrix(0, n, K)
   for (i in 1:n) {
-    for (j in 1:min(29+i, 80)){
+    for (j in 1:min(29 + i, 80)) {
       if (30 + i - j > 0 && 30 + i - j <= n) {
-        X[i,] <- X[i,] + X_tilde[30 + i - j, ] * pd[j]
+        X[i, ] <- X[i, ] + X_tilde[30 + i - j, ] * pd[j]
       }
     }
   }
   
   S <- crossprod(diff(diag(K), diff = 2))
-  list(X_tilde = X_tilde, X = X, S = S)
+  list(X_tilde = X_tilde, X = X, S = S, pd = pd)
 }
 
-### Question 2
+t <- 1:200
+K <- 80
+out <- modelling(t, K)
+dim(out$X_tilde)
+print(out)
+
+##Q2
+
 
